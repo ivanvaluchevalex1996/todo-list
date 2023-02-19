c91b9e1e7fe7a50d33cd724ac9b85ae08b7d487d
ivan <ivanvaluchevalex1996@gmail.com>
2023-02-19 23:04:17 +0300

    change func (clearCompleted)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,14 +3,15 @@ import TaskFilter from "../TaskFilter/TaskFilter";
 import "./Footer.css";
 import PropTypes from "prop-types";
 
-function Footer({ filtered, onFilterChange, count, clearCompleted, todos }) {
+function Footer({ filtered, onFilterChange, count, clearCompleted }) {
   return (
     <footer className="footer">
       <span className="todo-count">{count} items left</span>
       <div className="filters">
         <TaskFilter filtered={filtered} onFilterChange={onFilterChange} />
       </div>
-      <button type="button" onClick={() => clearCompleted(todos)}>
+      {/* <button type="button" onClick={() => clearCompleted(todos)}> */}
+      <button type="button" onClick={clearCompleted}>
         Clear completed
       </button>
     </footer>
